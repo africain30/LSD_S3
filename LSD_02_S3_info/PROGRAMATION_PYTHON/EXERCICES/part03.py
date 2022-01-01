bae3ef64867c6b924911d2ffc9e310ae1a65b6ba
africain30 <80362737+africain30@users.noreply.github.com>
2022-01-01 11:23:57 +0100

    Update part03.py

--- LSD_02_S3_info/PROGRAMATION_PYTHON/EXERCICES/part03.py
+++ LSD_02_S3_info/PROGRAMATION_PYTHON/EXERCICES/part03.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
 
+pd.set_option("display.max.rows",None)
+
+file = pd.set_option("display.max.rows",na_values = np.array(['?','n.a']))
+
+#EXERCICE 01
+file.haid(5)
+file.tail(5)
+
+#
