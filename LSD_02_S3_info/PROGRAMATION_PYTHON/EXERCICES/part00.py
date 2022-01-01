c5d146f84d90b361e93a3c3cd377c312d684b93b
africain30 <80362737+africain30@users.noreply.github.com>
2022-01-01 12:18:08 +0100

    Update part00.py

--- LSD_02_S3_info/PROGRAMATION_PYTHON/EXERCICES/part00.py
+++ LSD_02_S3_info/PROGRAMATION_PYTHON/EXERCICES/part00.py
@@ -1,19 +1,28 @@
-#exercice 02
+#CHARAF YOUSRI
+#LSD_2 : S3
+#MODULE : INFORMATIQUE
+#LE 02/DEC/2021
+
+
+#EXERCICE 00
+name = input("Enter your name ")
+print("hello", name)
+
+
+#EXERCICE 02
 def sum_mult(a,b):
   print("le produit de  ",a, "et " ,b, "est : ", a*b)
   print("la somme de  ",a, "et " ,b, "est : ", a+b)
-
 sum_mult(5,3)
 print("-----------------------------------------")
 
 
-
-#exercice 03
+#EXERCICE 03
 print('Name', 'Is', 'James', sep='**')
 print("-----------------------------------------")
 
 
-#exercice 04
+#EXERCICE 04
 i=1
 while(i<11):
   print(i,'\n')
@@ -21,7 +30,7 @@ while(i<11):
 print("-----------------------------------------")
 
 
-#exercice 05
+#EXERCICE 05
 a = ' '
 for i in range(6):
 	a = a +' {} '.format(i)
@@ -30,7 +39,7 @@ print("-----------------------------------------")
 
 
 
-#exercice 06
+#EXERCICE 06
 def tablle_mult(a):
   for i in range(13):
     print(i," * ",a," = ",a*i,'\n')
@@ -39,7 +48,7 @@ print("-----------------------------------------")
 
 
 
-#exercice 07
+#EXERCICE 07
 def test_product(a,b):
   if(a*b<1000):
     print( a," + ",b," = ",a+b)
@@ -50,7 +59,7 @@ print("-----------------------------------------")
 
 
 
-#exercice 08
+#EXERCICE 08
 def check_true(a):
   if(a[len(a)-1] == a[0]):
     print(True)
