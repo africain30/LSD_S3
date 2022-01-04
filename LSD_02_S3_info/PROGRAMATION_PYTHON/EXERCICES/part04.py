ca8d9f88763b21efe1b9d3ff8ea3ece9e91e11dc
africain30 <80362737+africain30@users.noreply.github.com>
2022-01-04 12:13:50 +0100

    Update part04.py

--- LSD_02_S3_info/PROGRAMATION_PYTHON/EXERCICES/part04.py
+++ LSD_02_S3_info/PROGRAMATION_PYTHON/EXERCICES/part04.py
@@ -4,7 +4,6 @@
 # CHARAF YOUSRI 
 # 04/DEC/2022
 
-
 #EXERCICE 01
 class square:
 	def __init__(self , l , L):  
@@ -16,6 +15,7 @@ class square:
 		return (self.l)**2
 	def perimeter(self):
 		return (self.l)*4
+
 	
 #EXERCICE 02
 class rectangle(square):
@@ -27,12 +27,23 @@ class rectangle(square):
     return (self.L) * (self.l)
   def perimeter_rec(self):
     return (self.l + self.L) * 2 
-
-
-
 a = square( 10 , 10 )
 b = rectangle( 10 , 10 )
 print( "the area of the square is : "  , a.area() )
 print( "the perimeter of the square is : "  , a.perimeter() )
 print( "the area of the rectangle  is : "  , a.area_rec() )
 print( "the perimeter of the rectagle is : "  , a.perimeter_rec() )
+
+
+#EXERCICE 03
+class SampleClass:
+
+    def __init__(self, a):
+        self.__a = a
+    @property
+    def a(self):
+        return self.__a
+x = SampleClass(3)
+print(x.a)
+x = SampleClass(23)
+print(x.a)
