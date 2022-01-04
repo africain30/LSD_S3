b4cdfe8d601b8ceb25a175a2d937c5b0719fbec7
africain30 <80362737+africain30@users.noreply.github.com>
2022-01-04 12:12:07 +0100

    Create oython_oop.py

---
+++ LSD_02_S3_info/PROGRAMATION_PYTHON/EXERCICES/oython_oop.py
@@ -0,0 +1,17 @@
+# LSD 2 : S3
+# CHARAF YOUSRI 
+# LE 04/01/2022
+
+#EXERCICE 01
+#Explain the reference of variables :
+
+# Person : the class 
+# person : variable of type Person (it is an object)
+# surname : is a carracterestique of the variable person
+# age :  is the age of the object "person"
+# AGE : is a methode of the class "Person" that returns the age of "person"
+# self.email =  
+# person.email = 
+
+
+#EXERCICE 02
