439a841858a134e91a92b7115b5c7bb96ba42678
africain30 <80362737+africain30@users.noreply.github.com>
2022-01-06 12:00:46 +0100

    Update oython_oop.py

--- LSD_02_S3_info/PROGRAMATION_PYTHON/EXERCICES/oython_oop.py
+++ LSD_02_S3_info/PROGRAMATION_PYTHON/EXERCICES/oython_oop.py
@@ -15,3 +15,35 @@
 
 
 #EXERCICE 02
+
+#EXERCICE 03
+#EXERCICE 04
+class number():
+  MULTIPLIER  = 5
+  def __init__(self,x,y):
+    if type(x) != int() or type(y) != int():
+      self.x = x
+      self.y = y
+    else:
+      raise ValueError;
+  
+  def add(self):
+    return self.x + self.y
+
+  def multiply(self, a):
+        return self.MULTIPLIER * a
+  
+  def subtract(b,c):
+        return b-c
+
+  def value(self):
+        return self.x, self.y
+
+
+num = number(5,6)
+print(num.add())
+print(num.multiply(2))
+print(num.subtract(2,4))
+print(num.value())
+
+#EXERCICE 05
